3b2c9bf6b5321113cc2e4a9ff4cacb3f597d4707
William Mckee <williamcmckee@protonmail.com>
2023-05-20 21:17:07 +1200

    add type to pages instead of import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,137 @@
 import getCar from './car';
-import { CarPlay } from './CarTypes';
 import Link from 'next/link';
+
+export interface CarPlay {
+  idh: Idh;
+}
+
+export interface Idh {
+  plate:            string;
+  query_type:       string;
+  created:          number;
+  is_trade_plate:   boolean;
+  odometer_history: any[];
+  header:           Header;
+  vehicle:          Vehicle;
+}
+
+export interface Header {
+  not_found:      string;
+  not_assigned:   boolean;
+  errors:         Errors;
+  not_available:  boolean;
+  is_trade_plate: boolean;
+}
+
+export interface Errors {
+  "58146": string;
+}
+
+export interface Vehicle {
+  vehicle_system_id:                             string;
+  year_of_manufacture:                           string;
+  make:                                          string;
+  model:                                         string;
+  submodel:                                      string;
+  body_style:                                    string;
+  vehicle_type:                                  string;
+  vin:                                           string;
+  chassis:                                       string;
+  engine_no:                                     string;
+  cc_rating:                                     number;
+  power:                                         number;
+  main_colour:                                   string;
+  second_colour:                                 string;
+  reported_stolen:                               string;
+  country_of_origin:                             string;
+  assembly_type:                                 string;
+  gross_vehicle_mass:                            number;
+  reliable_odometer:                             string;
+  maximum_rated_towed_mass_for_unbraked_trailer: number;
+  maximum_rated_towed_mass_for_braked_trailer:   number;
+  tare_weight:                                   number;
+  no_of_axles:                                   number;
+  axle_type:                                     string;
+  front_axle_group_rating:                       number;
+  rear_axle_group_rating:                        number;
+  date_of_first_registration_in_nz:              number;
+  cause_of_latest_registration:                  string;
+  subject_to_wof:                                string;
+  subject_to_wof_inspection:                     string;
+  subject_to_cof:                                string;
+  subject_to_cof_inspection:                     string;
+  subject_to_ruc:                                string;
+  date_of_last_registration:                     number;
+  imported_damaged:                              string;
+  registered_overseas:                           string;
+  number_of_owners:                              null;
+  no_of_seats:                                   number;
+  fuel_type:                                     string;
+  alternative_fuel_type:                         string;
+  inspection_agent:                              string;
+  wheelbase:                                     number;
+  vehicle_usages:                                VehicleUsage[];
+  vehicle_usage:                                 string;
+  registration_status:                           string;
+  cancellation_reason_code:                      string;
+  previous_country_of_registration:              string;
+  year_of_first_registration_overseas:           number;
+  month_of_first_registration_overseas:          number;
+  day_of_first_registration_overseas:            number;
+  cancellation_date_of_registration:             boolean;
+  plate:                                         string;
+  replacement_plate:                             string;
+  plate_type:                                    string;
+  plates:                                        Plate[];
+  date_of_latest_wof_inspection:                 string;
+  result_of_latest_wof_inspection:               string;
+  expiry_date_of_last_successful_wof:            string;
+  date_of_latest_cof_inspection:                 number;
+  result_of_latest_cof_inspection:               string;
+  expiry_date_of_last_successful_cof:            number;
+  inspections:                                   Inspection[];
+  licences:                                      Licence[];
+  continuous_licence:                            string;
+  licence_type:                                  string;
+  licence_expiry_date:                           number;
+  date_of_issue_for_latest_licence:              number;
+  time_of_issue_for_latest_licence:              string;
+  inconsistent_odometer:                         string;
+  latest_odometer_reading:                       number;
+  latest_daily_usage:                            number;
+  reported_stolen_nzta:                          string;
+}
+
+export interface Inspection {
+  latest:      string;
+  type:        string;
+  date:        number;
+  result:      string;
+  expiry_date: number;
+  btn:         string;
+}
+
+export interface Licence {
+  number:             string;
+  licence_type:       string;
+  continuous_licence: string;
+  expiry_date:        number;
+  issue_datetime:     number;
+}
+
+export interface Plate {
+  plate_status:       string;
+  plate_type:         string;
+  registration_plate: string;
+  effective_date:     number;
+}
+
+export interface VehicleUsage {
+  vechile_usage_code: string;
+  vehicle_usage_code: string;
+  effective_date:     number;
+}
+
 
 export default async function Home(Car: CarPlay) {
   const plateArray = ['360J', 'APY771', 'W0L0JBF68Y7051756', '11UPL', '12PC', 
